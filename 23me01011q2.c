0d047784d34c48f795cf8320f6a1641659ac08ad
yellownikki96 <149192495+yellownikki96@users.noreply.github.com>
2023-11-10 02:16:48 +0530

    23me01011q2.c

--- 23me01011q2.c
+++ 23me01011q2.c
@@ -1,36 +1,30 @@
-#include<stdio.h>
-struct time
-{
-    int hours;
-    int min;
-    int sec;
-};
-int main(){
-    struct time time1,time2,temp;
-    printf("Enter the first time in hours minutes and seconds format: ");
-    scanf("%d%d%d",&time1.hours,&time1.min,&time1.sec);
-    printf("Enter the second time in hours minutes and seconds format: ");
-    scanf("%d%d%d",&time2.hours,&time2.min,&time2.sec);
-    if ((time1.hours<time2.hours) || ((time1.hours==time2.hours)&&(time1.min<time2.min)) || ((time1.hours==time2.hours)&&(time1.min==time2.min)&&(time1.sec<time2.sec)))
-    {
-        temp=time1;
-        time1=time2;
-        time2=temp;
-    }
-    
-    int t1=0,t2=0,t3=0;
-    t3=time1.sec-time2.sec;
-    if (t3<0)
-    {
-        t3+=60;
-        time1.min--;
-    }
-    t2=time1.min-time2.min;
-    if (t2<0)
-    {
-        t2+=60;
-        time1.hours--;
-    }
-    t1=time1.hours-time2.hours;
-    printf("The time differnce is %dhours %dminutes %dseconds",t1,t2,t3);
-}
+#include <stdio.h>
+#include <stdlib.h>
+
+ struct time
+ {
+     int hour,minute,second;
+ };
+ 
+ void  main()
+ {
+     struct time time1,time2;
+     int second1,second2,diff;
+     printf("enter 1st time in hours , minutes and seconds\n ");
+     scanf("%d%d%d",&time1.hour,&time1.minute,&time1.second);
+     
+     printf("enter 2nd time in hours , minutes and seconds\n ");
+     scanf("%d%d%d",&time2.hour,&time2.minute,&time2.second);
+     
+     if(time1.hour<0 || time1.minute<0 || time1.second<0 ||time2.hour<0 || time2.minute<0 || time2.second<0 )
+     {
+         printf("invalid input");
+         exit(0);
+     }
+     
+     second1=time1.hour*3600+time1.minute*60+time1.second;
+     second2=time2.hour*3600+time2.minute*60+time2.second;
+     diff=abs(second1-second2);
+     printf("time difference is : %d hours , %d minutes, %d seconds ",diff/3600,(diff%3600)/60,(diff%3600)%60);
+ }
+ 
